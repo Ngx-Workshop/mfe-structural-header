8adb3620a79bb00ef92d680e2e5425e84d2db1a8
Wesley DuSell <dusellfam@gmail.com>
2025-08-08 12:57:16 -0400

    testing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'ngx-header-mfe',
-  imports: [MatButtonModule],
+  imports: [MatIcon, MatButtonModule],
   template: `
-    <h1>Welcome to {{ title }} Testing Change!</h1>
-    <a mat-flat-button >Testing</a>
+    <nav class="docs-navbar-header">
+      <a mat-button routerLink="/">
+        <mat-icon>connecting_airports</mat-icon>Ngx MFE Orchestrator
+      </a>
+      <div class="flex-spacer"></div>
+      <ngx-theme-picker></ngx-theme-picker>
+    </nav>
   `,
-  styles: [],
+  styles: [
+    `
+      :host {
+        color: var(--mat-sys-on-primary-container);
+        background-color: var(--mat-sys-primary-container);
+        box-shadow: var(--mat-sys-level5);
+        .docs-navbar-header {
+          display: flex;
+          flex-wrap: wrap;
+          align-items: center;
+          padding: 0.5em 1em;
+          mat-icon {
+            font-size: 2rem;
+            width: 2rem;
+            height: 2rem;
+            margin: 0 0.1em 0.1875em 0;
+            vertical-align: middle;
+          }
+        }
+      }
+    `,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class App {
   protected title = 'ngx-header-mfe';
