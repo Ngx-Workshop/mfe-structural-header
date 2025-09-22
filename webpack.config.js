d46fa7b3d8cc9998774168b554a6e03ba1d7a1c3
Wesley DuSell <dusellfam@gmail.com>
2025-09-21 23:16:39 -0400

    Update navi list package

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ module.exports = withModuleFederationPlugin({
     '@tmdjr/ngx-navigational-list': {
       singleton: true,
       strictVersion: true,
-      requiredVersion: '0.0.3',
+      requiredVersion: '0.0.4',
     },
   },
 });
