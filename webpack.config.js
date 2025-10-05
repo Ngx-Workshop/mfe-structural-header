ff181c07b33cbb60650f00c9b830630e06d9f330
Wesley DuSell <dusellfam@gmail.com>
2025-10-05 12:39:30 -0400

    Bump navi list

--- webpack.config.js
+++ webpack.config.js
@@ -63,11 +63,10 @@ module.exports = withModuleFederationPlugin({
       requiredVersion: '0.0.2',
     },
 
-    //"@tmdjr/ngx-navigational-list": "0.0.1",
     '@tmdjr/ngx-navigational-list': {
       singleton: true,
       strictVersion: true,
-      requiredVersion: '0.0.5',
+      requiredVersion: '0.0.6',
     },
   },
 });
